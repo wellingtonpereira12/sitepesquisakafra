10a97387c602a42f8c89f8a7be7cf1875d2fd041
Wellington Pereira <lleco12@gmail.com>
2024-05-19 16:14:28 -0300

    alteração consulta api

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -2,31 +2,56 @@ import React, { useState } from "react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import * as C from "./styles";
-import { Link, useNavigate } from "react-router-dom";
-import useAuth from "../../contexts/hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
-  const { signin } = useAuth();
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState(""); // Alterado de 'email' para 'username'
   const [senha, setSenha] = useState("");
   const [error, setError] = useState("");
 
-  const handleLogin = () => {
-    if (!email | !senha) {
+  const handleLogin = async () => {
+    if (!username || !senha) {
       setError("Preencha todos os campos");
       return;
     }
 
-    const res = signin(email, senha);
+    try {
 
-    if (res) {
-      setError(res);
-      return;
+      const response = await fetch("https://teste-api-5421.onrender.com/loginkafra", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password: senha}), // Alterado de 'email' para 'username'
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao fazer login. Tente novamente.");
+      }
+
+      const data = await response.json();
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      console.log(data);
+
+      // Login bem-sucedido, redirecione para a página inicial ou faça outras ações necessárias
+      if (data === true) {
+        // Armazena o token de autenticação no localStorage
+        localStorage.setItem("token", "seu_token_aqui");
+      
+        // Redireciona para a página inicial ou faça outras ações necessárias
+        navigate("/home");
+      } else {
+        throw new Error("Credenciais inválidas. Verifique seu nome de usuário e senha.");
+      }
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      setError("Erro ao fazer login. Tente novamente mais tarde.");
     }
-
-    navigate("/home");
   };
 
   return (
@@ -34,16 +59,22 @@ const Signin = () => {
       <C.Label>Faça o login</C.Label>
       <C.Content>
         <Input
-          type="email"
-          placeholder="Digite seu Nome"
-          value={email}
-          onChange={(e) => [setEmail(e.target.value), setError("")]}
+          type="text" // Alterado de 'email' para 'text'
+          placeholder="Digite seu nome de usuário" // Alterado de 'email' para 'nome de usuário'
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+            setError("");
+          }}
         />
         <Input
           type="password"
-          placeholder="Digite sua Senha"
+          placeholder="Digite sua senha"
           value={senha}
-          onChange={(e) => [setSenha(e.target.value), setError("")]}
+          onChange={(e) => {
+            setSenha(e.target.value);
+            setError("");
+          }}
         />
         <C.labelError>{error}</C.labelError>
         <Button Text="Entrar" onClick={handleLogin} />
